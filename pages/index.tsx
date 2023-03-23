71849ff37bdb685cca44e3e77f0b95d7597091bb
Nursultan <nursultan@MacBook-Pro-Nursultan.local>
2023-03-23 19:01:51 +0500

    module #8

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { Htag, Button } from '../components';
+import React, { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
-      <Htag tag='h1'>Текст</Htag>
-      <Button appearance='primary' className='asdad'>Кнопка</Button>
-      <Button appearance='ghost'>Кнопка</Button>
+      <Htag tag='h1'>Заголовок</Htag>
+      <Button appearance='primary' arrow='right'>
+        Кнопка
+      </Button>
+      <Button appearance='ghost' arrow='down'>
+        Кнопка
+      </Button>
+      <P size='l'>Большой</P>
+      <P>Средний</P>
+      <P size='s'>Маленький</P>
+      <Tag size='s'>Ghost</Tag>
+      <Tag size='m' color='red'>
+        Red
+      </Tag>
+      <Tag size='s' color='green'>
+        Green
+      </Tag>
+      <Tag color='primary'>Green</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
