d1349447c77b74aa93a2736cc3628fe3656be568
Nursultan <nursultan@MacBook-Pro-Nursultan.local>
2023-03-26 14:02:42 +0500

    product page

---
+++ components/Card/Card.props.ts
@@ -0,0 +1,8 @@
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+
+export interface CardProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  color?: 'white' | 'blue';
+
+  children: ReactNode;
+}
---
+++ page-components/TopPageComponent/TopPageComponent.props.ts
@@ -0,0 +1,12 @@
+import {
+  TopLevelCategory,
+  TopPageModel,
+} from '../../interfaces/page.interface';
+
+import { ProductModel } from '../../interfaces/product.interface';
+
+export interface TopPageComponentProps {
+  firstCategory: TopLevelCategory;
+  page: TopPageModel;
+  products: ProductModel[];
+}
